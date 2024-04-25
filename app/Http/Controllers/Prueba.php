3ce6a4eee0773c2ae5f60927979338172707f673
journey1010 <ginopaflo001608@gmail.com>
2024-04-25 12:46:45 -0500

    Add FIRST DEV BRANCH

---
+++ app/Http/Controllers/Prueba.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Prueba extends Controller
+{
+    public function test()
+    {
+        $a = 'a';
+        return;
+    }
+}
