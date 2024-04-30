b0dce562cc1591eb90bf06bc13a0efc4daa7fd19
journey1010 <ginopaflo001608@gmail.com>
2024-04-30 14:48:08 -0500

    Add comentarios y Tabla de contrataciones

---
+++ database/migrations/2024_04_30_175620_create_contrataciones_obra_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contrataciones_obra', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('obra_id')->constrained('obras');
+            $table->json('contraciones')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contrataciones_obra', function (Blueprint $table){
+            $table->dropConstrainedForeignId('obra_id');
+        });
+        Schema::dropIfExists('contrataciones_obra');
+    }
+};
