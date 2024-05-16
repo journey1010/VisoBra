bc68bd2d079d552f1921fb5d25613b45663c1c91
journey1010 <ginopaflo001608@gmail.com>
2024-05-16 01:53:30 -0500

    Add Redirect route for /

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [AuthController::class, 'index']);
 Route::get('/login', [AuthController::class, 'index']);
 Route::post('/auth/horizon/dashboard', [AuthController::class, 'login'])->name('login.dashboard');
