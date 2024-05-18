23375ab22dc0046c9ec8074a35f0d74a2e98b699
journey1010 <ginopaflo001608@gmail.com>
2024-05-18 00:02:48 -0500

    Add Job for poblar geobra table

--- app/Jobs/ProcessPoblarGeobra.php
+++ app/Jobs/ProcessPoblarGeobra.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Exceptions\DataHandlerException;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -13,6 +14,7 @@ use App\Services\GeobraEndpoint;
 use App\Services\Notify;
 use App\Services\Reporting;
 use App\Services\Mailer;
+use Illuminate\Support\Facades\Http;
 
 class ProcessPoblarGeobra implements ShouldQueue
 {
@@ -34,18 +36,54 @@ class ProcessPoblarGeobra implements ShouldQueue
 
     public function handle(): void
     {
-        try{
-            for($i = 1; $i <= 2; $i++){
-                
+        try {
+            $geobra = new GeobraEndpoint();
+            $http = new HttpClient();
+            $this->configureHttpClient($http, $geobra);
+    
+            $response = $this->fetchValidResponse($http, $geobra);
+    
+            if ($response === null) {
+                throw new DataHandlerException('Fallo al obtener datos para el codigo de inversion con id :' . $this->id);
             }
-        
-        }catch(\Exception $e){
-            $notifier = new Notify(new Mailer());
-            $notifier->clientNotify(
-                to: 'ginopalfo001608@gmail.com', 
-                message: $e->getMessage(),
-                subject: 'Fallo en visoobra al obtener datos');
-            Reporting::loggin($e, 100);
+    
+            $geobra->store($response);
+        } catch (\Exception $e) {
+            $this->handleException($e);
         }
     }
+    
+    private function configureHttpClient(HttpClient $http, GeobraEndpoint $geobra): void
+    {
+        $http->config(3, 100, 30, $geobra->headers);
+    }
+    
+    private function fetchValidResponse(HttpClient $http, GeobraEndpoint $geobra): ?array
+    {
+        for ($i = 0; $i <= 1; $i++) {
+            $response = $http->makeRequest(
+                $geobra->setUrl($i),
+                $geobra->method,
+                $geobra->params
+            );
+    
+            if ($geobra->validateFormat($response)) {
+                return $response;
+            }
+        }
+        return null;
+    }
+    
+    private function handleException(\Exception $e): void
+    {
+        $notifier = new Notify(new Mailer());
+        $notifier->configLimiter(3, 'Geobra');
+        $notifier->clientNotify(
+            to: 'ginopalfo001608@gmail.com',
+            message: $e->getMessage(),
+            subject: 'Fallo en visoobra al obtener datos'
+        );
+        Reporting::loggin($e, 100);
+    }
+    
 }
