dda7cf62edefb88976658aeea91d5ae3e322568f
journey1010 <ginopaflo001608@gmail.com>
2024-05-19 18:08:32 -0500

    Add Model for foto obra

---
+++ app/Models/Fotos.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Fotos extends Model
+{
+    use HasFactory;
+
+    protected $table  = 'fotos_obra';
+
+    protected $fillable = [
+        'obra_id', 
+        'files_path',
+    ];
+
+    protected function contrataciones(): Attribute
+    {
+        return Attribute::make(
+            set: fn (array $value) => json_encode($value),
+            get: fn (string $value) => json_decode($value, true),
+        );
+    }
+}
