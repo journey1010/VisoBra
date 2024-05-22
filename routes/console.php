2003eeb52a4f5e76d5caebb0f05c257fae19a7b2
dev <dev@dev.dev.com>
2024-05-22 08:31:51 -0500

    Add Schedule task for Automatization

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,25 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+use App\Console\Commands\CleanLogs;
+use App\Console\Commands\UpdateContrataciones;
+use App\Console\Commands\UpdateFotos;
+use App\Console\Commands\UpdateObra;
+use App\Console\Commands\SearchNewObras;
+
+Schedule::command(SearchNewObras::class)
+          ->weekly()
+          ->timezone('America/Lima');
+Schedule::command(UpdateContrataciones::class)
+          ->weeklyOn(1, '01:00')
+          ->timezone('America/Lima');
+Schedule::command(UpdateFotos::class)
+          ->weeklyOn(1, '01:00')
+          ->timezone('America/Lima');
+Schedule::command(UpdateObra::class)
+          ->weeklyOn(1, '01:00')
+          ->timezone('America/Lima');
+Schedule::command(CleanLogs::class)
+          ->weeklyOn(3, '01:00')
+          ->timezone('America/Lima');
