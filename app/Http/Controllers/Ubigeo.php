c202ad149bf82bd93bddd8db0fdb192d0b993347
dev <dev@dev.dev.com>
2024-05-31 10:25:49 -0500

    Add endpoi

--- app/Http/Controllers/Ubigeo.php
+++ app/Http/Controllers/Ubigeo.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Ubigeo\Ubigeo as UbigeoRequest;
+use App\Services\Ubigeo as Ub;
+use Exception;
 
 class Ubigeo extends Controller
 {
-    //
+    public function departments(UbigeoRequest $request)
+    {
+        try{
+            $result = Ub::departments($request->name, $request->id); 
+            return response()->json([
+                'items' => $result
+            ], 200);
+        }catch(Exception $e){
+            return response()->json([
+                'message' => 'Ocurrio un problema al consultar los datos'
+            ], 500);
+        }
+    }
+
+    public function provinces(UbigeoRequest $request)
+    {
+        try{
+            $result = Ub::provinces($request->name, $request->id); 
+            return response()->json([
+                'items' => $result
+            ], 200);
+        }catch(Exception $e){
+            return response()->json([
+                'message' => 'Ocurrio un problema al consultar los datos'
+            ], 500);
+        }
+    }
+
+    public function districts(UbigeoRequest $request)
+    {
+        try{
+            $result = Ub::districts($request->name, $request->id); 
+            return response()->json([
+                'items' => $result
+            ], 200);
+        }catch(Exception $e){
+            return response()->json([
+                'message' => 'Ocurrio un problema al consultar los datos'
+            ], 500);
+        }
+    }
 }
---
+++ app/Http/Requests/Ubigeo/Ubigeo.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Ubigeo;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class Ubigeo extends FormRequest
+{
+    protected $stopOnFirstFailure = true;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $json = new JsonResponse([
+            'message' => messageValidation($validator),
+        ], 422);
+        throw new HttpResponseException($json);
+    }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'nullable|string',
+            'id' =>  'nullable|integer'
+        ];
+    }
+}
