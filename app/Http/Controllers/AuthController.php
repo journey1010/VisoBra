b4825808dde674629a30fa5a8e9e318bd60701ac
dev <dev@dev.dev.com>
2024-06-17 12:24:05 -0500

    fix: auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use App\Http\Requests\Auth\Horizon;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -18,16 +20,22 @@ class AuthController extends Controller
 
     public function login(Horizon $request): JsonResponse
     {
-        $credentials = $request->only('email','password');
-        $credentials['status'] = true;
-        if(Auth::attempt($credentials, true)){
-            $request->session()->regenerate();
-            
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        $user = User::where('email', $credentials['email'])->first();
+
+        if ($user && Hash::check($credentials['password'], $user->password)) {
+            Auth::login($user);
             return response()->json([
                 'status' => 'success',
                 'message' => 'Credenciales correctas'
             ], 200);
         }
+
         return response()->json([
             'status' => 'error',
             'message' => 'El usuario no esta autorizado'
