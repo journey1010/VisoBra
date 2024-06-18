512c7dd371f7307cdcba994c8387a070091051c3
journey1010 <ginopaflo001608@gmail.com>
2024-06-17 19:51:20 -0500

    fix: proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,15 +21,16 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
         $middleware->alias([
             'ipAllowed' => IpAlloweb::class
         ]);
-        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::HEADER_X_FORWARDED_AWS_ELB
-    );
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
